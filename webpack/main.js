1704e3894c72a6c94085eeac49c869cb861b93f1
Keaton Burleson <keaton.burleson@me.com>
2019-01-31 15:54:05 -0600

    More JS, new header

--- webpack/main.js
+++ webpack/main.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
-import Project from './classes/project.js';
 import ProjectManager from './classes/project-manager.js';
+import LazyLoad from 'vanilla-lazyload';
 
-window.jQuery = $;
-window.$ = $;
+window.$ = window.jQuery = $;
 
 $(document).ready(function(){
+
     let junkRepos = ['IntelliJSettings', 'COMP-2150'];
     let projectManager = new ProjectManager("128keaton", "110f4bd9cef118020fb114cc32972297c321fd64", junkRepos);
     projectManager.fetchRepos().then(function(projects){
@@ -13,7 +13,14 @@ $(document).ready(function(){
         $.each(projects, function(i, project){
             $('.pinned-repos-list').append(project.getListItem());
         });
-    })
+    });
+
+    new LazyLoad({
+        elements_selector: ".lazy-load",
+        to_webp: true
+    });
+
+
 
 });
 
