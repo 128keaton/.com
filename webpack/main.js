c282f7bd6c1dec7c7bf3db16b0aa609249f6b7b2
128keaton <keaton.burleson@me.com>
2019-02-01 14:52:49 +0000

    Updating to 128keaton/128keaton.com@8b63aa1416afd73271e757fb064d3f1c92a37f8d.

--- webpack/main.js
+++ webpack/main.js
@@ -1,20 +1,47 @@
 import $ from 'jquery';
 import ProjectManager from './classes/project-manager.js';
 import LazyLoad from 'vanilla-lazyload';
-
+import axios from 'axios';
+import 'babel-polyfill';
 
 window.$ = window.jQuery = $;
 
 $(document).ready(function () {
 
-    let junkRepos = ['IntelliJSettings', 'COMP-2150'];
-    let projectManager = new ProjectManager("128keaton", process.env.GH_TOKEN, junkRepos);
-    projectManager.fetchRepos().then(function (projects) {
-        $('.no-data').hide();
-        $.each(projects, function (i, project) {
-            $('.pinned-repos-list').append(project.getListItem());
+    const requestToken = async () => {
+        try {
+            return await axios.get('https://stuff.128keaton.com/sec/r.php')
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+    const fetchRepoWithToken = async (token) => {
+        let junkRepos = ['IntelliJSettings', 'COMP-2150'];
+        let projectManager = new ProjectManager("128keaton", token, junkRepos);
+
+        projectManager.fetchRepos().then(function (projects) {
+            $('.no-data').hide();
+            $.each(projects, function (i, project) {
+                $('.pinned-repos-list').append(project.getListItem());
+            });
         });
-    });
+    };
+
+    const getToken = async () => {
+        if (process.env.GH_TOKEN) {
+            console.log("development mode: " + process.env.GH_TOKEN);
+            return fetchRepoWithToken(process.env.GH_TOKEN);
+        } else {
+            const tokenRequest = await requestToken();
+
+            if (tokenRequest.data.key) {
+                return fetchRepoWithToken(tokenRequest.data.key);
+            }
+        }
+    };
+
+    getToken();
 
     new LazyLoad({
         elements_selector: ".lazy-load",
