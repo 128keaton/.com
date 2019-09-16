f1905d999fed8576d94f99227205f18ea7aead82
Keaton Burleson <keaton.burleson@me.com>
2019-09-16 14:15:39 -0500

    Adds some lovely additional languages :D
    Signed-off-by: Keaton Burleson <keaton.burleson@me.com>

--- webpack-scripts/main.js
+++ webpack-scripts/main.js
@@ -1,13 +1,15 @@
 import $ from 'jquery';
 import ProjectManager from './classes/project-manager.js';
+import CodeSnippets from './classes/code-snippets.js';
 import LazyLoad from 'vanilla-lazyload';
 import 'babel-polyfill';
 
 window.$ = window.jQuery = $;
 
 $(document).ready(function () {
-
     $(".projects-wrapper").slideDown();
+    let codeSnippets = new CodeSnippets('.hljs');
+    let lastScroll = 0;
 
     let projectManager = new ProjectManager("128keaton", ['IntelliJSettings', 'COMP-2150']);
     projectManager.fetchRepos().then(function (projects) {
@@ -18,21 +20,30 @@ $(document).ready(function () {
         });
     });
 
+    codeSnippets.setInitialSnippet();
 
-    async function showProject(i){
-            $("#project-" + i + " .pinned-repo-item-content").delay(100 * i).fadeIn({
-                start: function () {
-                    $(this).css({
-                        display: "flex"
-                    })
-                }
-            });
+    async function showProject(i) {
+        $("#project-" + i + " .pinned-repo-item-content").delay(100 * i).fadeIn({
+            start: function () {
+                $(this).css({
+                    display: "flex"
+                })
+            }
+        });
     }
 
+    $(window).on("scroll resize", function () {
+        const scrollOffset = $(window).scrollTop();
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+        if (scrollOffset >= (lastScroll + 50)) {
+            codeSnippets.showNextSnippet();
+            lastScroll = scrollOffset;
+        } else if (scrollOffset <= (lastScroll - 50)) {
+            codeSnippets.showLastSnippet();
+            lastScroll = scrollOffset;
+        }
+    });
+
 
     new LazyLoad({
         elements_selector: ".lazy-load",
